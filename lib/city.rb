391faf9abf8753416195afa6fd43c496237a2645
Grayson Wright <wright.grayson@gmail.com>
2013-09-16 16:22:19 -0400

    Improve constructors and accessors for cities
    Allow construction by passing in an options hash

--- lib/city.rb
+++ lib/city.rb
@@ -1,14 +1,37 @@
 class City
-  def initialize(string)
-    @name = string.split("\t").first
-    @longitude = string.split(/[\(,\)]/)[3]
-    @latitude = string.split(/[\(,\)]/)[4]
-    @hub = string.chars.first == '*'
+  def initialize(options)
+    @name = options[:name]
+    @longitude = options[:longitude]
+    @latitude = options[:latitude]
+    @hub = options[:hub]
+    @data = options[:data]
   end
 
-  attr_accessor :name, :longitude, :latitude
+  def name
+    @name ||= data.split("\t").first
+  end
 
   def hub?
-    @hub
+    @hub ||= data.chars.first == '*'
+  end
+
+  def latitude
+    @latitude ||= location.last
+  end
+
+  def longitude
+    @longitude ||= location.first
+  end
+
+  private
+
+  attr_accessor :data
+
+  def location
+    @location ||= begin
+                    location_string = data.split("\t").last
+                    coord_strings = location_string.scan(/\d+/)
+                    coord_strings.map(&:to_i)
+                  end
   end
 end
--- test/city_test.rb
+++ test/city_test.rb
@@ -2,39 +2,54 @@ require_relative '../lib/city'
 require 'minitest/autorun'
 
 describe City do
-  it 'can be created with a data string' do
-    City.new "Omaha(NE)\t\t(367,209)"
+  describe 'initialization' do
+    it 'can be created with a data string' do
+      City.new data: "Omaha(NE)\t\t(367,209)"
+    end
+
+    it 'can be created with individual arguments' do
+      City.new name: "Gotham", longitude: 304, latitude: 192, hub: true
+    end
+
+    it 'cannot be created with no arguments' do
+      lambda {
+        City.new
+      }.must_raise ArgumentError
+    end
   end
 
-  it 'cannot be created with no arguments' do
-    lambda {
-      City.new
-    }.must_raise ArgumentError
+  describe 'when instantiated with a string' do
+    it 'sets the city name' do
+      omaha.name.must_equal "Omaha(NE)"
+    end
+
+    it 'sets the city longitude' do
+      omaha.longitude.must_equal 367
+    end
+
+    it 'sets the city latitude' do
+      omaha.latitude.must_equal 209
+    end
+
+    it 'recognizes hub cities' do
+      omaha.hub?.must_equal false
+      miami.hub?.must_equal true
+    end
+
+    private
+    def omaha
+      @_omaha = City.new data: "Omaha(NE)\t\t(367,209)"
+    end
+
+    def miami
+      @_miami = City.new data: "*Miami(FL)\t\t(626,443)"
+    end
   end
 
-  it 'sets the city name' do
-    omaha.name.must_equal "Omaha(NE)"
-  end
-
-  it 'sets the city longitude' do
-    omaha.longitude.must_equal "367"
-  end
-
-  it 'sets the city latitude' do
-    omaha.latitude.must_equal "209"
-  end
-
-  it 'recognizes hub cities' do
-    omaha.hub?.must_equal false
-    miami.hub?.must_equal true
-  end
-
-  private
-  def omaha
-    @_omaha = City.new "Omaha(NE)\t\t(367,209)"
-  end
-
-  def miami
-    @_miami = City.new "*Miami(FL)\t\t(626,443)"
+  describe 'when instantiated with a hash and data string' do
+    it 'overwrites data string values with explicit hash values' do
+      city = City.new name: 'Gotham', data: "Omaha(NE)\t\t(367,209)"
+      city.name.must_equal 'Gotham'
+    end
   end
 end
