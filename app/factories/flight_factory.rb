91e1627d9ad2ce4d32e6a57f3a97de261c67da31
Grayson Wright <wright.grayson@gmail.com>
2013-09-16 19:17:27 -0400

    Import flights from a data file

---
+++ app/factories/flight_factory.rb
@@ -0,0 +1,25 @@
+class FlightFactory
+  def initialize(cities)
+    @cities = cities
+  end
+
+  def import_from_file(path)
+    flights = []
+    file = File.new path
+    file.each { |line|
+      origin_index = file.lineno - 1
+      line.split.each_with_index do |cost, destination_index|
+        if cost.to_i != 0
+          flights.push Flight.new cities[origin_index], cities[destination_index], cost.to_i
+        end
+      end
+    }
+    if file.lineno > cities.count
+      raise 'Not enough cities provided.'
+    end
+    flights
+  end
+
+  private
+  attr_accessor :cities
+end
